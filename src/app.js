1b73ba1438a67c12fe98cba612f394dc880b72dc
Chuck "MANCHUCK" Reeves <chuck.reeves@nfrastructure.com>
2019-02-27 13:21:40 -0500

    Initial commit
    Initial commit along with basic format for v2 documentation

---
+++ src/app.js
@@ -0,0 +1,18 @@
+/**
+ * @fileOverview Main express bootstrap
+ */
+const express = require('express');
+const app = express();
+const swaggerUi = require('swagger-ui-express');
+
+app.use('/v1', express.static(`${__dirname}/../data/docs/v1`));
+app.use('/v2', express.static(`${__dirname}/../data/docs/v2`));
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(null, {
+    swaggerUrls: [
+        {url: '/v2/openapi.json', name: 'Version 2 API'},
+    ],
+    explorer: true,
+}));
+
+module.exports = app;
