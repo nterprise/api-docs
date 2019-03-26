83e9a4e88f23e8bded1d9b2a03c6b814591953a3
Chuck "MANCHUCK" Reeves <chuck.reeves@nfrastructure.com>
2019-03-26 14:41:48 -0400

    Adding security
    Updated the endpoint to be API to allows for easier mapping when
    deployed to production

--- src/app.js
+++ src/app.js
@@ -4,11 +4,17 @@
 const express = require('express');
 const app = express();
 const swaggerUi = require('swagger-ui-express');
+const staticOptions = {
+    cacheControl: true,
+    etag: true,
+    maxAge: '2 days',
+    immutable: true
+};
 
-app.use('/v1', express.static(`${__dirname}/../data/docs/v1`));
-app.use('/v2', express.static(`${__dirname}/../data/docs/v2`));
+app.use('/v1', express.static(`${__dirname}/../data/docs/v1`, staticOptions));
+app.use('/v2', express.static(`${__dirname}/../data/docs/v2`, staticOptions));
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(null, {
+app.use('/api', swaggerUi.serve, swaggerUi.setup(null, {
     swaggerUrls: [
         {url: '/v1/openapi.json', name: 'Version 1 API'},
         {url: '/v2/openapi.json', name: 'Version 2 API'},
